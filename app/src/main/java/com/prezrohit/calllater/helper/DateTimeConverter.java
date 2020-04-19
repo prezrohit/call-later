3c902dbf99d79265a13733014189c644d24acf59
prezrohit <rohitsr80@gmail.com>
2020-04-19 15:15:19 +0530

    Completed Database Setup

--- app/src/main/java/com/prezrohit/calllater/helper/DateTimeConverter.java
+++ app/src/main/java/com/prezrohit/calllater/helper/DateTimeConverter.java
@@ -1,10 +1,10 @@
-package com.codencolors.callscheduler.helper;
+package com.prezrohit.calllater.helper;
 
 import androidx.room.TypeConverter;
 
 import java.util.Date;
 
-public class Converters {
+public class DateTimeConverter {
     @TypeConverter
     public static Date fromTimestamp(Long value) {
         return value == null ? null : new Date(value);
@@ -13,5 +13,32 @@ public class Converters {
     @TypeConverter
     public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
+    }
+
+    public static String convertTo12Hour(int hour, int minute) {
+        String timeSet;
+
+        if (hour > 12) {
+            hour -= 12;
+            timeSet = "PM";
+
+        } else if (hour == 0) {
+            hour += 12;
+            timeSet = "AM";
+
+        } else if (hour == 12) {
+            timeSet = "PM";
+
+        } else {
+            timeSet = "AM";
+        }
+
+        String min;
+        if (minute < 10)
+            min = "0" + minute;
+        else
+            min = String.valueOf(minute);
+
+        return String.valueOf(hour) + ':' + min + " " + timeSet;
     }
 }
--- app/src/main/java/com/prezrohit/calllater/helper/DateTimeHelper.java
+++ app/src/main/java/com/prezrohit/calllater/helper/DateTimeHelper.java
@@ -1,4 +1,35 @@
 package com.prezrohit.calllater.helper;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class DateTimeHelper {
+	public static String formatDate(String stringDate) {
+		DateFormat originalFormat = new SimpleDateFormat("dd MM yyyy", Locale.ENGLISH);
+		DateFormat targetFormat = new SimpleDateFormat("dd MMM YYYY", Locale.ENGLISH);
+		Date date = null;
+		try {
+			date = originalFormat.parse(stringDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return targetFormat.format(date);
+	}
+
+	public static Date getDateFromString(String dateTimeAsString) {
+		String dateAsString = dateTimeAsString.split(" - ")[0];
+		String timeAsString = dateTimeAsString.split(" - ")[1];
+
+		try {
+			DateFormat sourceFormat = new SimpleDateFormat("dd MMM yyyy hh:mm a");
+			return sourceFormat.parse(dateAsString + " " + timeAsString);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
