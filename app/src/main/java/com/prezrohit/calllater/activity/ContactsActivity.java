03abaaaf2557cc8dc08a5c4f118c1aaf013e979f
prezrohit <rohitsr80@gmail.com>
2020-04-19 14:10:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/prezrohit/calllater/activity/ContactsActivity.java
@@ -0,0 +1,16 @@
+package com.prezrohit.calllater.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prezrohit.calllater.R;
+
+public class ContactsActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contacts);
+	}
+}
